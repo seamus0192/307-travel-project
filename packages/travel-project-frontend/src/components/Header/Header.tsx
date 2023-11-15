51f4a324cc3d1f3233dfd350692a24e4034ec6f9
Seamus Q O'Malley <63891206+seamus0192@users.noreply.github.com>
2023-11-15 11:29:42 -0800

    this is a nightmare

--- packages/travel-project-frontend/src/components/Header/Header.tsx
+++ packages/travel-project-frontend/src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 import {
   AppBar,
   Toolbar,
   Button,
   Typography,
   IconButton,
-  Box,
-} from "@mui/material";
-import { Link as RouterLink } from "react-router-dom";
-import VacationTrackerLogo from "../../assets/vacation_tracker.jpg";
+  Box
+} from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
+import VacationTrackerLogo from '../../assets/vacation_tracker.jpg'
 
 const Header: React.FC = () => {
   return (
@@ -18,11 +18,11 @@ const Header: React.FC = () => {
       elevation={0}
       sx={{
         borderBottom: (theme) => `1px solid ${theme.palette.divider}`,
-        overflow: "hidden",
+        overflow: 'hidden'
       }}
     >
-      <Toolbar sx={{ flexWrap: "wrap", justifyContent: "space-between" }}>
-        <Box sx={{ display: "flex", alignItems: "center", mr: 2 }}>
+      <Toolbar sx={{ flexWrap: 'wrap', justifyContent: 'space-between' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', mr: 2 }}>
           <IconButton component={RouterLink} to="/home">
             <img
               src={VacationTrackerLogo}
@@ -36,53 +36,39 @@ const Header: React.FC = () => {
             color="inherit"
             noWrap
             sx={{
-              display: "flex",
-              alignItems: "center",
-              fontFamily: "'Roboto', sans-serif",
+              display: 'flex',
+              alignItems: 'center',
+              fontFamily: '\'Roboto\', sans-serif',
               fontWeight: 500,
-              marginLeft: 1,
+              marginLeft: 1
             }}
           >
             Vacation Tracker
           </Typography>
         </Box>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
           <Button
             variant="contained"
             sx={{
-              backgroundColor: "#7139a8",
-              ":hover": {
-                bgcolor: "#965ad3",
+              backgroundColor: '#7139a8',
+              ':hover': {
+                bgcolor: '#965ad3'
               },
-              mx: 1,
+              mx: 1
             }}
             component={RouterLink}
             to="/login"
           >
             Login / Sign Up
           </Button>
-          {/* <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#7139a8",
-              ":hover": {
-                bgcolor: "#965ad3",
-              },
-              mx: 1,
-            }}
-            component={RouterLink}
-            to="/references"
-          >
-            References
-          </Button> */}
           <Button
             variant="contained"
             sx={{
-              backgroundColor: "#7139a8",
-              ":hover": {
-                bgcolor: "#965ad3",
+              backgroundColor: '#7139a8',
+              ':hover': {
+                bgcolor: '#965ad3'
               },
-              mx: 1,
+              mx: 1
             }}
             component={RouterLink}
             to="/create-itinerary"
@@ -92,11 +78,11 @@ const Header: React.FC = () => {
           <Button
             variant="contained"
             sx={{
-              backgroundColor: "#7139a8",
-              ":hover": {
-                bgcolor: "#965ad3",
+              backgroundColor: '#7139a8',
+              ':hover': {
+                bgcolor: '#965ad3'
               },
-              mx: 1,
+              mx: 1
             }}
             component={RouterLink}
             to="/itinerary/day"
@@ -106,39 +92,7 @@ const Header: React.FC = () => {
         </Box>
       </Toolbar>
     </AppBar>
-  );
-};
-
-export default Header;
-
-
-// OG Code
-
-/* 
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom';
-
-
-function Header() {
-    return (
-        <div className={styles['header']}>
-            <Link to={"/home"}>
-                <img src= "../../assets/vaction_tracker.jpg" alt="Vacation Tracker logo" width="200" height="200" />
-                <header>Vacation Tracker</header>
-            </Link>
-            <a className={styles['header-button']}>
-                References
-            </a>
-            <Link to='/create-itinerary' className={styles['header-button']}>
-                Create Itinerary
-            </Link>
-            <Link to='/login' className={styles['header-button']}>
-                Login/ Sign Up
-            </Link>
-        </div>
-   );
+  )
 }
 
-export default Header;
-
-*/
+export default Header
